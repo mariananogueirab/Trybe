f42c750fca48b401be0e3df2273876f9329c623b
mariananogueirab <mariananbp@gmail.com>
2021-05-19 22:06:50 -0300

    Adiciona exercicio 7

---
+++ trybe-exercises/Fundamentos/Bloco 04/4.1/exercise_7.js
@@ -0,0 +1,19 @@
+let nota = -1;
+
+if (nota >= 90 && nota <= 100) {
+    console.log('Sua nota é A');
+} else if (nota >= 80 && nota < 90) {
+    console.log('Sua nota é B');
+} else if (nota >= 70 && nota < 80) {
+    console.log('Sua nota é C');
+} else if (nota >= 60 && nota < 70) {
+    console.log('Sua nota é D');
+} else if (nota >= 50 && nota <60) {
+    console.log('Sua nota é E');
+} else if (nota < 50 && nota >= 0) {
+    console.log('Sua nota é F');
+}else {
+    console.log('Nota inválida, o programa será encerrado');
+}
+
+// Transforma uma nota em porcentagem em ABCDEF
