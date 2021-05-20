02e3f3ff10b15b4e49e437816d070c003846abde
mariananogueirab <mariananbp@gmail.com>
2021-05-20 19:24:29 -0300

    Exercicios 8 e 9

--- trybe-exercises/Fundamentos/Bloco 04/4.2/exercicio_1-7.js
+++ trybe-exercises/Fundamentos/Bloco 04/4.2/exercicio_1-7.js
@@ -1,12 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+// 1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
 for (let index = 0; index < numbers.length; index +=1) {
     console.log(numbers[index]);
 }
 
-// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// 2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
 let sum = 0;
 
@@ -16,12 +16,12 @@ for (let index1 = 0; index1 < numbers.length; index1 += 1) {
 
 console.log(sum);
 
-// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+// 3 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 
 let media = sum / numbers.length
 console.log(media)
 
-// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+// 4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
 if (media > 20) {
     console.log('Valor maior que 20');
