4f763eedeab6b5489a4412cb5c825ad3d8daf5f1
mariananogueirab <mariananbp@gmail.com>
2021-05-20 19:15:11 -0300

    Exercicios de 1 a 7

---
+++ trybe-exercises/Fundamentos/Bloco 04/4.2/exercicio_1-7.js
@@ -0,0 +1,62 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+
+for (let index = 0; index < numbers.length; index +=1) {
+    console.log(numbers[index]);
+}
+
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+let sum = 0;
+
+for (let index1 = 0; index1 < numbers.length; index1 += 1) {
+    sum = sum + numbers[index1];
+}
+
+console.log(sum);
+
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let media = sum / numbers.length
+console.log(media)
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+if (media > 20) {
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor ou igual a 20');
+}
+
+// 5 - Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let maiorValor = 0;
+for (let index2 = 0; index2 < numbers.length; index2 +=1) {
+    if (numbers[index2] > maiorValor) {
+        maiorValor = numbers[index2];
+    }
+}
+console.log(maiorValor)
+
+
+// 6 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+let numImpar = 0;
+for (let index3 = 0; index3 < numbers.length; index3 +=1) {
+    if (numbers[index3]%2 != 0) {
+        numImpar+=1
+    }
+}
+
+console.log(numImpar)
+
+// 7 - Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+let menorValor = maiorValor;
+for (let index4 = 0; index4 < numbers.length; index4 +=1) {
+    if (numbers[index4] < menorValor) {
+        menorValor = numbers[index4];
+    }
+}
+console.log(menorValor)
+
