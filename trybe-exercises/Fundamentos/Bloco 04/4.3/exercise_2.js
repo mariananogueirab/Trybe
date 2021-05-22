fdee9c637e503cba9d5362d8928d8c2fb3ce6409
mariananogueirab <mariananbp@gmail.com>
2021-05-22 19:57:51 -0300

    Exercicio 3 certo

--- trybe-exercises/Fundamentos/Bloco 04/4.3/exercise_2.js
+++ trybe-exercises/Fundamentos/Bloco 04/4.3/exercise_2.js
@@ -1,9 +1,12 @@
 // 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
 
-let vetor = [];
+let linha = '';
 let n = 5;
 
 for (let index =0; index < n; index += 1) {
-   vetor[index] = '*';
-   console.log(vetor);
+    for (let index2 =0; index2 < index +1; index2 += 1) {
+        linha = linha + '*';
+}
+console.log(linha);
+linha = '';
 }
