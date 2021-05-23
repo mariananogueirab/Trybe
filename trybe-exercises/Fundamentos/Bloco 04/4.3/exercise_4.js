612283f39fbc963c9fbd222f8552226d03ea546e
mariananogueirab <mariananbp@gmail.com>
2021-05-23 19:31:37 -0300

    Exercicio 4 super certo

--- trybe-exercises/Fundamentos/Bloco 04/4.3/exercise_4.js
+++ trybe-exercises/Fundamentos/Bloco 04/4.3/exercise_4.js
@@ -1,21 +1,24 @@
 let n = 5;
 let ast = '';
 let espaco = '';
+let subEspaco = espaco.substring();
 let linha = '';
 let nLinha = Math.ceil(n/2);
 
 for(let index = 0; index < nLinha; index += 1) {
-    if (index <= 0) {
+    if (index === 0) {
         ast = '*';
        for (indexEsp = (n-1)/2; indexEsp > 0; indexEsp -= 1) {
-            espaco = espaco + ' ';
+            espaco = espaco + '-';
         } 
-    } else if (index > 0 && index < nLinha) {
-        ast = ast + '*' + '*';
-        espaco = - ' ';
     }
-    if (index >= nLinha) {
-        espaco = '';
+    if (index > 0 && index < nLinha) {
+        ast = ast + '*' + '*';
+        if (index >= nLinha-1) {
+            espaco = '';
+        } else {
+            espaco = espaco.substring(0,espaco.length-1);
+    }
     }
     linha = espaco + ast + espaco
     console.log(linha);
