25b57f7e702c670b3bbd7c3f60cab3bba8740f3b
mariananogueirab <mariananbp@gmail.com>
2021-06-12 22:15:31 -0300

    Validando nome e email

--- trybe-exercises/Fundamentos/Bloco 06/6.1/script.js
+++ trybe-exercises/Fundamentos/Bloco 06/6.1/script.js
@@ -33,23 +33,49 @@ TO: "Tocantins",
 for (let key in estadosBR) {
   const option = document.createElement('option');
   option.innerText = estadosBR[key];
+  option.value = key;
   estados.appendChild(option);
 }
 
 let nome = document.querySelector('#input-name');
+
+nome.addEventListener('keyup', () => {
+  let nomeVerify = nome.value.trim();
+  console.log(nomeVerify.length)
+  if (nomeVerify.length > 0 && nomeVerify.length < 40) {
+    return nome.value; 
+  }
+  alert('Nome Inválido');
+})
+
 let email = document.querySelector('#input-email');
+
+email.addEventListener('keyup', () => {
+  let emailVerify = email.value.trim();
+  console.log(emailVerify);
+  console.log(emailVerify.match(/@/g));
+  if (emailVerify.length > 0 && emailVerify.length < 50) {
+    return emailVerify;
+  } 
+    alert('E-mail inválido');
+})
+
 let cpf = document.querySelector('#input-cpf');
 let endereco = document.querySelector('#input-end');
 let cidade = document.querySelector('#input-cidade');
 let estado = document.querySelector('#input-estado');
 let typeHouse = document.getElementsByName('casa-apt');
-// tipo radio pega o input pelo name.
-
+let resumo = document.querySelector('#resumo-curriculo');
+let cargo = document.querySelector('#input-cargo');
+let descricaoCargo = document.querySelector('#descricao-cargo');
 let dataInicio = document.querySelector('#data-inicio');
 let buttonEnviar = document.querySelector('#button-submit');
+let buttonLimpar = document.querySelector('#button-limpar')
+
 buttonEnviar.addEventListener('click', (event) => {
   event.preventDefault();
-  let nomeOk = false;
+
+/*   let nomeOk = false;
   let data = dataInicio.value;
   let dia = parseInt(data.slice(0,2));
   let mes = parseInt(data.slice(3,5));
@@ -60,7 +86,7 @@ buttonEnviar.addEventListener('click', (event) => {
     
   if (dia > 0 && dia <= 31 && mes > 0 && mes <= 12 && ano > 0) {}  
         
-  }
+  } */
   
 })
 
