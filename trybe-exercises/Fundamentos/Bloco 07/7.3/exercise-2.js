403583ea31ac589d372e6b2531896cece84ea402
mariananogueirab <mariananbp@gmail.com>
2021-06-21 19:06:21 -0300

    Exercicios 1 e 2

---
+++ trybe-exercises/Fundamentos/Bloco 07/7.3/exercise-2.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4])
