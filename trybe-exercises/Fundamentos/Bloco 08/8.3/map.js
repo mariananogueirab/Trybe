e174f1f14eb46c90a82c173247cc946afaf79a8d
mariananogueirab <mariananbp@gmail.com>
2021-06-26 16:13:34 -0300

    Exercicios

---
+++ trybe-exercises/Fundamentos/Bloco 08/8.3/map.js
@@ -0,0 +1,42 @@
+/* const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = [];
+
+for (let index = 0; index < persons.length; index += 1) {
+  fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+}
+
+console.log(fullNames); */
+
+
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
