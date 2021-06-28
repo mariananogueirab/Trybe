5ae6dcf6274b1b6abb543b1489fa78b2f08e1f1f
mariananogueirab <mariananbp@gmail.com>
2021-06-28 20:57:53 -0300

    Exercicio 4

--- trybe-exercises/Fundamentos/Bloco 08/8.4/exercicio4.js
+++ trybe-exercises/Fundamentos/Bloco 08/8.4/exercicio4.js
@@ -63,4 +63,21 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+// Encontre o livro com o maior nome
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  return books.reduce((acc, book) => acc.name.length > book.name.length ? acc : book)
+}
+console.log(longestNamedBook())
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
