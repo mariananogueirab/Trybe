c0c947cf480a6adee0e577ef62e7623b2c23fc22
mariananogueirab <mariananbp@gmail.com>
2021-07-07 15:43:58 -0300

    Exercicios 9.1

---
+++ trybe-exercises/Fundamentos/Bloco 09/9.1/erros-exercise.js
@@ -0,0 +1,30 @@
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onError) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5; // o número aleatório é menor ou igual à 0.5? Se sim retorna true, se não retorna false.
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country); // Parâmetro da função, que quando chamar vai colocar o countryName
+    }
+    else {
+      const errorMessage = "Country could not be found";
+      onError(errorMessage); // Parâmetro da função, que quando chamar vai colocar o printErrorMessage
+    }
+  }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
