000415aadae041ca1eee5fb7c490b7b560c8cf7c
mariananogueirab <mariananbp@gmail.com>
2021-07-25 17:36:05 -0300

    Exemplo do conteudo

---
+++ trybe-exercises/Front-End/Bloco 12/12.1/exercises12.1/src/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+class Button extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      numeroDeCliques: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    /* Passando uma callback à função setState, que recebe de parâmetros
+    o estado anterior e as props do componente, você garante que as atualizações
+    do estado acontecerão uma depois da outra! O _ quer dizer que não vai usar a props. */
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
+  }
+}
+
+export default Button;
